c545ad81224b3c7f23f7cb9317b00d263b85ac23
Shinha <shinha97@gmail.com>
2017-10-21 12:27:53 -0700

    Added user input support and cleaned up js code

--- popup.js
+++ popup.js
@@ -1,28 +1,32 @@
-function click(e){
-	chrome.tabs.executeScript(null,
-	{code:"document.body.style.backgroundImage='url(" +images[e.target.id]
-	+ "'"});
-	window.close();
-}
-
-document.addEventListener('DOMContentLoaded',function() {
+document.addEventListener('DOMContentLoaded',function() { //Big brother is always listening
 	var divs = document.querySelectorAll('div');
 	document.getElementById("click1").addEventListener("click", myFunction);
 });
 
 
-function myFunction() {
-	    document.getElementById("demo").innerHTML = "Hello World";
-	    alert("Whoa");
-	}
+// function click(e){
+// 	chrome.tabs.executeScript(null,
+// 	{code:"document.body.style.backgroundImage='url(" +images[e.target.id]
+// 	+ "'"});
+// 	window.close();
+// }
 
- chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+
+
+
+function myFunction() {
+	 //alert(x);
+	 chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
      var activeTab = arrayOfTabs[0];	//Only one active tab in existence
      var activeTabId = activeTab.id; 
-     document.getElementById("demo").innerHTML = activeTab.url;
+     document.getElementById("url").innerHTML = "You are currently at:" + activeTab.url;          //Will return the URL of the current active tab.
+     var user_input = document.getElementById("search").value;
+     document.getElementById("demo").innerHTML = user_input;
+      
 
 
   });
+}
 
  chrome.tabs.getCurrent(function(tab){
  	document.getElementById("demo").innerHTML = tab.url;
