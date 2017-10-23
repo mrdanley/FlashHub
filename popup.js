ae529461ef0a8501b9e4ef621fd73c6a2483c2b8
Daniel <dle1@cpp.edu>
2017-10-23 16:01:11 -0700

    removed api usage from popup.js

--- popup.js
+++ popup.js
@@ -20,50 +20,8 @@ function myFunction() {
 
 		 //Will return the URL of the current active tab.
 		 document.getElementById("url").innerHTML = "You are currently at: " + activeTab.url;
-		 document.getElementById("demo").innerHTML = NLU_API(user_input);
+		 document.getElementById("demo").innerHTML = user_input;
   });
-}
-
-function NLU_API(input){
-	//create variable to use api
-	var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
-
-	//allows user access to api
-	var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-		'username': '6a94dba3-2dfc-4ebd-9061-14321e40b4f3',
-		'password': 'KlGVUtjP5Nb8',
-		'version_date': '2017-02-27'
-	});
-	//create parameters for nlu to user in intepretation
-	var parameters = {
-		//'url': 'http://www.rollingstone.com/glixel/news/league-of-legends-developers-are-working-on-a-new-game-w508720',
-	 'text': input,
-		'features': {
-			'emotion': {},
-			'sentiment' : {},
-			'keywords' : {
-				'emotion' : true,
-				'sentiment' : true,
-				'limit' : 5
-			}
-		}
-	}
-	var output = "before test";	//api output variable
-	natural_language_understanding.analyze(parameters, function(err, response) {
-		if (err)
-			output = ('API error:'+err);
-		else{
-			output = ("Score: "+response['sentiment']['document']['score']+'\n'+
-								 "Label: "+response['sentiment']['document']['label']+'\n\n'+
-								 "Sadness: "+response['emotion']['document']['emotion']['sadness']+'\n'+
-								 "Joy: "+response['emotion']['document']['emotion']['joy']+'\n'+
-								 "Fear: "+response['emotion']['document']['emotion']['fear']+'\n'+
-								 "Disgust: "+response['emotion']['document']['emotion']['disgust']+'\n'+
-								 "Anger: "+response['emotion']['document']['emotion']['anger']+'\n');
-		 }
-	});
-
-	return output;
 }
 
 chrome.tabs.getCurrent(function(tab){
