692da7be9a944032846ab811cc78015e80c047d9
Daniel <dle1@cpp.edu>
2017-10-23 16:03:34 -0700

    cleaned up api_test.js

--- api_test.js
+++ api_test.js
@@ -1,12 +1,15 @@
-var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+function nlu_call(){
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js);
+//allows user access to api
 var natural_language_understanding = new NaturalLanguageUnderstandingV1({
   'username': '6a94dba3-2dfc-4ebd-9061-14321e40b4f3',
   'password': 'KlGVUtjP5Nb8',
   'version_date': '2017-02-27'
 });
-
+//create parameters for nlu to user in intepretation
 var parameters = {
-  'url': 'http://www.rollingstone.com/glixel/news/league-of-legends-developers-are-working-on-a-new-game-w508720',
+  //'url': 'http://www.rollingstone.com/glixel/news/league-of-legends-developers-are-working-on-a-new-game-w508720',
+ 'text': "asdasdasdawd dadadad asdasd adsasd",
   'features': {
     'emotion': {},
     'sentiment' : {},
@@ -17,19 +20,20 @@ var parameters = {
     }
   }
 }
-
+var output = "";
 natural_language_understanding.analyze(parameters, function(err, response) {
   if (err)
-    console.log('error:', err);
-  else
-    console.log(JSON.stringify(response,null,2));
-  /*
-    console.log("Score: "+response['sentiment']['document']['score']);
-    console.log("Label: "+response['sentiment']['document']['label']+'\n');
-    console.log("Sadness: "+response['emotion']['document']['emotion']['sadness']);
-    console.log("Joy: "+response['emotion']['document']['emotion']['joy']);
-    console.log("Fear: "+response['emotion']['document']['emotion']['fear']);
-    console.log("Disgust: "+response['emotion']['document']['emotion']['disgust']);
-    console.log("Anger: "+response['emotion']['document']['emotion']['anger']);
-    */
+    output = ('API error:'+err);
+  else{
+    output = ("Score: "+response['sentiment']['document']['score']+'\n'+
+               "Label: "+response['sentiment']['document']['label']+'\n\n'+
+               "Sadness: "+response['emotion']['document']['emotion']['sadness']+'\n'+
+               "Joy: "+response['emotion']['document']['emotion']['joy']+'\n'+
+               "Fear: "+response['emotion']['document']['emotion']['fear']+'\n'+
+               "Disgust: "+response['emotion']['document']['emotion']['disgust']+'\n'+
+               "Anger: "+response['emotion']['document']['emotion']['anger']+'\n');
+   }
 });
+return output;
+
+}
