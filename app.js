86ac5ea0f3387f71a5474897dfea26be7ec00c30
Daniel <dle1@cpp.edu>
2017-11-07 18:52:13 -0800

    extension waits 14 seconds for analytics before load

--- app.js
+++ app.js
@@ -5,17 +5,52 @@ document.addEventListener('DOMContentLoaded',function() {
 });
 
 function runSearch(){
+	document.getElementById('results').innerHTML = "";
   var search = document.getElementById('search').value;
+	var xmlhttp = new XMLHttpRequest();
+	post(xmlhttp,search);
+	setTimeout(function() { get(xmlhttp); },14000);
+}
 
-  xmlhttp = new XMLHttpRequest();
+function post(xmlhttp,search) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    xmlhttp.open('POST', "http://localhost:8001/poststring", true);
 
-  xmlhttp.open("POST","http://localhost:8001/poststring", true);
-  xmlhttp.send(search);
-	xmlhttp.open("GET","http://localhost:8001/getstring", true);
-  xmlhttp.onreadystatechange=function(){
-     if (xmlhttp.readyState==4 && xmlhttp.status==200){
-       document.getElementById('results').innerHTML = xmlhttp.responseText;
-     }
-  }
-  xmlhttp.send();
+    // Handle network errors
+    xmlhttp.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    xmlhttp.send(search);
+  });
+}
+
+function get(xmlhttp) {
+  // Return a new promise.
+  return new Promise(function(resolve, reject) {
+    // Do the usual XHR stuff
+    xmlhttp.open('GET', "http://localhost:8001/getstring", true);
+    xmlhttp.onload = function() {
+      // This is called even on 404 etc
+      // so check the status
+      if (xmlhttp.readyState==4 && xmlhttp.status == 200) {
+				document.getElementById('results').innerHTML = xmlhttp.responseText;
+        resolve(xmlhttp.response);
+      }
+      else {
+        reject(Error(xmlhttp.statusText));
+      }
+    };
+
+    // Handle network errors
+    xmlhttp.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    xmlhttp.send();
+  });
 }
--- server.js
+++ server.js
@@ -1,14 +1,61 @@
 //analytics array
-var analytics = [],
-    http = require('http'),
+var http = require('http'),
     fs = require('fs'),
     url = require('url'),
     feed = require('rss-to-json');
 
+//access ibm watson nlu api
+var NaturalLanguageUnderstandingV1,
+    natural_language_understanding,
+    analytics = {};
+
+//convert search to rss and convert to json
+function rss2json(searchString){
+  var rss = 'https://news.google.com/news/rss/search/section/q/'+searchString+'/'+searchString+'?hl=en&gl=US&ned=us';
+  console.log("Converting rss, "+rss+"...");
+  feed.load(rss,function(err,json){
+      console.log("Converted json...");
+      useNLU(json);
+  });
+  //callback();
+}
+//get sentiment and emotions for first 10 articles
+function useNLU(jsonItem){
+  var urlStartIndex;
+  var parameters;
+  NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+  natural_language_understanding = new NaturalLanguageUnderstandingV1({
+    'username': '6a94dba3-2dfc-4ebd-9061-14321e40b4f3',
+    'password': 'KlGVUtjP5Nb8',
+    'version_date': '2017-02-27'
+  });
+
+  for(var i=1, apiIndex = 0; i<jsonItem["items"].length; i++){
+    //making sure link is https secure
+    urlStartIndex = jsonItem["items"][i]["url"].lastIndexOf('url=')+1;
+
+    parameters = {
+      'url': jsonItem["items"][i]["url"],
+      'features': {
+        'emotion': {},
+        'sentiment': {}
+      }
+    };
+      natural_language_understanding.analyze(parameters, function(err, response) {
+        if (err){
+          //console.log("error: "+err);
+        }
+        else{
+          analytics[apiIndex] = JSON.stringify(response,null,2)+"<br><br>";
+          console.log(analytics[apiIndex]);
+          apiIndex++;
+        }
+      });
+  }
+}
 
 //communicate with client-side javascript file
- http.createServer(function(request, response)
- {
+ http.createServer(function(request, response){
    var path = url.parse(request.url).pathname;
    if(path=="/poststring"){
        var requestData = '';
@@ -20,12 +67,12 @@ var analytics = [],
        });
 
        request.on('end', function () {
-          console.log('requestData: ', requestData);
-           useNLU(requestData);
+          console.log('Request data sent: ', requestData);
+          rss2json(requestData);
        });
     }else if(path=="/getstring"){
        response.writeHead(200, {"Content-Type": "text/plain", 'Access-Control-Allow-Origin' : '*'});
-       response.end(analytics);
+       response.end(JSON.stringify(analytics));
     }else{
       fs.readFile('./index.html', function(err, file) {
           if(err) {
@@ -38,46 +85,3 @@ var analytics = [],
     }
 }).listen(8001);
 console.log('server running...');
-
-//access ibm watson nlu api
-function useNLU(searchString){
-  var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
-  var natural_language_understanding = new NaturalLanguageUnderstandingV1({
-    'username': '6a94dba3-2dfc-4ebd-9061-14321e40b4f3',
-    'password': 'KlGVUtjP5Nb8',
-    'version_date': '2017-02-27'
-  });
-  //convert search to rss and convert to json
-  var json = rss2json(searchString);
-  console.log('json: ',json);
-  //get sentiment and emotions for first 10 articles
-  for(var i=0;i<1;i++){
-    var parameters = {
-      'url': json["items"][i]["url"],
-      'features': {
-        'emotion': {},
-        'sentiment' : {}
-      }
-    }
-    natural_language_understanding.analyze(parameters, function(err, response) {
-      if (err)
-        analytics[i] = 'error: '+ err;
-      else
-        analytics[i] =  json["items"][i]["title"]+'<br><br>'+
-                     "Score: "+response['sentiment']['document']['score']+'<br>'+
-                     "Label: "+response['sentiment']['document']['label']+'<br><br>'+
-                     "Sadness: "+response['emotion']['document']['emotion']['sadness']+'<br>'+
-                     "Joy: "+response['emotion']['document']['emotion']['joy']+'<br>'+
-                     "Fear: "+response['emotion']['document']['emotion']['fear']+'<br>'+
-                     "Disgust: "+response['emotion']['document']['emotion']['disgust']+'<br>'+
-                     "Anger: "+response['emotion']['document']['emotion']['anger']+'<br><br>';
-    });
-  }
-}
-
-function rss2json(searchString){
-  var rss = 'https://news.google.com/news?q='+searchString+'&output=rss';
-  feed.load(rss,function(err,json){
-      return json;
-  });
-}
