2a291eb3a2292ae72ba57a030a05440dba9729ea
Daniel <dle1@cpp.edu>
2017-11-28 10:50:41 -0800

    graphs and newspaper image are invisible until submit button clicked

--- app.js
+++ app.js
@@ -9,28 +9,45 @@ function runSearch(){
   var search = document.getElementById('search').value;
 	var xmlhttp = new XMLHttpRequest();
 	post(xmlhttp,search);
+
+	//data analytics timer countdown
+	var seconds_left = 14;
+	var interval = setInterval(function() {
+  	if (seconds_left > 0){
+        document.getElementById('results').innerHTML = "Analyzing search..."+(--seconds_left);
+     }
+ 	}, 1000);
+
 	setTimeout(function() { get(xmlhttp); },14000);
+
+	//sentiment variables
+	var positive, negative;
+
+	//sample values
+	positive=30;
+	negative=20;
+	//show sentiment graph
+	document.getElementById('sentimentGraph').style.height="300px";
+	document.getElementById('sentimentGraph').style.width="300px";
+	sentimentGraph(positive, negative);
 
 	//emotion variables
 	var sadness, joy, fear, disgust, anger;
-    
+
 	//sample values
 	sadness = 20;
 	joy = 20;
 	fear = 20;
 	disgust = 20;
 	anger = 20;
-        
-        graphLoad(sadness,joy,fear,disgust,anger);
-  
-        //sentiment variables
-	var positive, negative;
-    
-	//sample values
-	positive=30;
-        negative=20;
-        
-        graphBar(positive, negative );
+	//show emotion graph
+	document.getElementById('emotionGraph').style.height="300px";
+	document.getElementById('emotionGraph').style.width="300px";
+  emotionGraph(sadness,joy,fear,disgust,anger);
+
+	//show newspaper image
+	document.getElementById('newspaperImage').style.height="100%";
+	document.getElementById('newspaperImage').style.width="100%";
 }
 
 function post(xmlhttp,search) {
@@ -58,7 +75,10 @@ function get(xmlhttp) {
       // This is called even on 404 etc
       // so check the status
       if (xmlhttp.readyState==4 && xmlhttp.status == 200) {
-				document.getElementById('results').innerHTML = xmlhttp.responseText;
+				var x = xmlhttp.responseText;
+				x = x.replace(/\"{/g,"{");
+				x = x.replace(/\\"/g,'"');
+				document.getElementById('results').innerHTML = x;
         resolve(xmlhttp.response);
       }
       else {
@@ -77,12 +97,12 @@ function get(xmlhttp) {
 }
 
 //create emotion graph
-function graphLoad(sadness,joy,disgust,fear,anger) {
-	 var chart = new CanvasJS.Chart("chartContainer",
+function emotionGraph(sadness,joy,disgust,fear,anger) {
+	 var chart = new CanvasJS.Chart("emotionGraph",
 	 {
-                
-               
-                
+
+
+
                exportEnabled: true,
                animationEnabled: true,
                theme:"theme2",
@@ -112,14 +132,14 @@ function graphLoad(sadness,joy,disgust,fear,anger) {
  }
 
 //create sentiment graph
-function graphBar(positive, negative) {
-	var chart2 = new CanvasJS.Chart("chartContainer2",
+function sentimentGraph(positive, negative) {
+	var chart2 = new CanvasJS.Chart("sentimentGraph",
     {
         exportEnabled: true,
         animationEnabled: true,
         theme:"theme2",
         backgroundColor: "#f0f5f5",
-        
+
       title:{
         text: "Sentiment"
       },
@@ -129,7 +149,7 @@ function graphBar(positive, negative) {
                      dataPoints: [
                      { y: 10, label: "positive"},
                      { y: 201, label: "negative"},
-      
+
                      ]
       }
       ]
